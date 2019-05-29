6e527e1f818a3511604cecbed779d3d5b97f59ec
Aria <aria@remus>
2019-05-29 10:43:17 +0200

    add itertools to get all contacts

--- Network.py
+++ Network.py
@@ -1,8 +1,11 @@
 import networkx as nx 
 import mdtraj as md 
 import numpy as np
+import itertools
 
 chunks = md.iterload('/home/agheerae/PDB/prot_apo_sim1_s10.dcd', chunk=100, top='/home/agheerae/PDB/prot.prmtop')
 for chunk in chunks:
-    contacts = md.compute_contacts(chunk)
+    contacts = md.compute_contacts(chunk, contacts=list(itertools.combinations(range(0, 250), r=2)))
     print(contacts)
+            
+
