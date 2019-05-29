bfffef0f180c2059d4f6a546788af618ce8360ef
Aria GHEERAERT <aria.gheeraert@gmail.com>
2019-05-29 11:09:13 +0200

    Aborted Project

--- Network.py
+++ Network.py
@@ -6,6 +6,6 @@ import itertools
 chunks = md.iterload('/home/agheerae/PDB/prot_apo_sim1_s10.dcd', chunk=100, top='/home/agheerae/PDB/prot.prmtop')
 for chunk in chunks:
     contacts = md.compute_contacts(chunk, contacts=list(itertools.combinations(range(0, 250), r=2)))
-    print(contacts)
+    print(contacts[0].shape)
             
 
